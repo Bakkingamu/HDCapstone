f828c3c22172859080987037a3560fa93522bf5c
Bakkingamu <bakkingamu0@gmail.com>
2017-09-19 13:05:11 -0400

    Prototype 1 v1

--- src/main.java
+++ src/main.java
@@ -1,15 +1,71 @@
 /**
  * Created by Justin on 9/12/2017.
  */
+import com.google.auth.Credentials;
+//logentries
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
+//pdf
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import java.io.File;
+import org.apache.pdfbox.*;
+// Google Cloud
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import javax.imageio.ImageIO;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        //pdfbox
+        //https://stackoverflow.com/questions/23326562/
+        PDDocument document = PDDocument.load(new File("HELLO.pdf"));
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        BufferedImage bim = pdfRenderer.renderImageWithDPI(0,300, ImageType.RGB);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageIO.write(bim,"png",os);
+        //https://cloud.google.com/vision/docs/reference/libraries#client-libraries-install-java
+        ByteString imageByteString = ByteString.copyFrom(os.toByteArray());
+        //vision
+        try(ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imageByteString).build();
+            Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+            requests.add(request);
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    annotation.getAllFields().forEach((k, v) ->
+                            System.out.printf("%s : %s\n", k, v.toString()));
+                }
+            }
+        }
+        //Log Entries
         Logger logger = LoggerFactory.getLogger("LE");
-        logger.debug("Hello world.");
+        //logger.debug("Hello world.");
 
         // print internal state
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
